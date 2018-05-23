f262b94be035357351b5389f2e0021d5cda60488
DAPPERstats <dapperstats@gmail.com>
2018-05-23 01:13:11 -0700

    small steps forward

--- R/portalcasting.R
+++ R/portalcasting.R
@@ -1,14 +1,14 @@
 #' @importFrom dplyr bind_rows filter full_join group_by inner_join left_join 
 #'   right_join mutate rename select ungroup summarise summarize
-#' @importFrom forecast na.interp
+#' @importFrom forecast auto.arima ets na.interp
 #' @importFrom lubridate as_date is.Date month year
-#' @importFrom magrittr %>%
+#' @importFrom magrittr %>% extract2
 #' @importFrom portalr abundance download_observations fcast_ndvi 
 #'   get_future_moons ndvi weather
 #' @importFrom purrr map
 #' @importFrom RCurl getURL
 #' @importFrom rlang !! !!! .data quo quos
-#' @importFrom stats qnorm
+#' @importFrom stats AIC qnorm
 #' @importFrom utils read.csv read.table tail write.table
 #' @importFrom yaml as.yaml
 #'
