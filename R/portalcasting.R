f61bf93dfd8d11d134cdf9c2ef84f19e6ce1c660
DAPPERstats <dapperstats@gmail.com>
2018-05-23 00:40:11 -0700

    namespace tidying

--- R/portalcasting.R
+++ R/portalcasting.R
@@ -1,14 +1,15 @@
 #' @importFrom dplyr bind_rows filter full_join group_by inner_join left_join 
 #'   right_join mutate rename select ungroup summarise summarize
-#' @importFrom forecast na.interp
+#' @importFrom forecast auto.arima ets forecast na.interp
 #' @importFrom lubridate as_date is.Date month year
-#' @importFrom magrittr %>%
+#' @importFrom magrittr %>% extract2
 #' @importFrom portalr abundance download_observations fcast_ndvi 
 #'   get_future_moons ndvi weather
 #' @importFrom purrr map
 #' @importFrom RCurl getURL
 #' @importFrom rlang !! !!! .data quo quos
-#' @importFrom stats qnorm
+#' @importFrom stats AIC predict qnorm
+#' @importFrom tscount tsglm predict.tsglm
 #' @importFrom utils read.csv read.table tail write.table
 #' @importFrom yaml as.yaml
 #'
