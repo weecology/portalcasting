4295b92f4ea0d6f70619b9d3ffd1c64e24e25078
DAPPERstats <dapperstats@gmail.com>
2019-01-29 13:19:38 -0800

    more testing for prepare_models

--- tests/testthat/test-14-prepare_models.R
+++ tests/testthat/test-14-prepare_models.R
@@ -11,8 +11,27 @@ test_that("models", {
 
 test_that("write_model", {
   expect_error(write_model(1), "`options_model` is not")
+  expect_output(
+    write_model(AutoArima_options(dirtree(main = "testing_casting"))))
+  expect_silent(
+    write_model(
+      model_options(dirtree(main = "testing_casting"), name = NULL)))
 })
 
 test_that("model_template", {
   expect_error(write_model(1), "`options_model` is not")
+
+  temp1 <- model_template(
+              AutoArima_options(dirtree(main = "testing_casting")))
+  expect_is(temp1, "character")
+  expect_equal(length(temp1), 1)
+
+  temp2 <- model_template(pevGARCH_options(dirtree(main = "testing_casting")))
+  expect_is(temp2, "character")
+  expect_equal(length(temp2), 1)
+
+  temp3 <- model_template(AutoArima_options(dirtree(main = "testing_casting",
+                                             subs = subdirs(subs = "test"))))
+  expect_is(temp3, "character")
+  expect_equal(length(temp3), 1)
 })
