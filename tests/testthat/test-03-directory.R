0848167f70e1fc78f7ac51f16a84b65a63b06d04
DAPPERstats <dapperstats@gmail.com>
2019-01-28 22:56:40 -0800

    Update test-03-directory.R

--- tests/testthat/test-03-directory.R
+++ tests/testthat/test-03-directory.R
@@ -4,9 +4,7 @@ test_location <- "travis"
 
 test_that("setup_dir", {
   expect_error(setup_dir(1))
-  expect_silent(setup_dir(all_options(main = "ok", quiet = TRUE)))
-  unlink(dirtree(main = "ok"), recursive = TRUE, force = TRUE)
-  unlink(dirtree(main = "ok"), recursive = TRUE, force = TRUE)
+
   expect_message(setup_dir(all_options(main = "ok")))
   unlink(dirtree(main = "ok"), recursive = TRUE, force = TRUE)
   unlink(dirtree(main = "ok"), recursive = TRUE, force = TRUE)
