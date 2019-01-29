27aa625c3106dfd21d20700146c2121ceac34202
DAPPERstats <dapperstats@gmail.com>
2019-01-29 13:02:25 -0800

    documentation and testing for prepare_models functions

--- R/prepare_models.R
+++ R/prepare_models.R
@@ -1,25 +1,38 @@
 #' @title Provide the names of models
 #'
 #' @description Based on a \code{set}, returns a character vectory of model
-#'   names to be included. Currently only support for 
-#'   \code{set = "portalcasting"}.
+#'   names to be included. Currently only support for \code{set = "prefab"}.
 #'
-#' @param set type of model options (currently only support for "prefab")
+#' @param set \code{characher} value of the type of model (currently only 
+#'   support for \code{"prefab"}). Use \code{NULL} to build a custom set
+#'   from scratch via \code{add}.
 #'
-#' @param add character name of model to add to the list to run
+#' @param add \code{character} vector of name(s) of model(s) to add to the 
+#'   setup by \code{set}.
 #'
-#' @return vector of model names
+#' @return \code{models}-class \code{character} vector of model names.
 #'
 #' @export
 #'
 models <- function(set = "prefab", add = NULL){
+  if (!is.null(set) & !is.character(set)){
+    stop("`set` is not NULL or a character")
+  }
+  if (length(set) > 1){
+    stop("`set` can only be of length = 1")
+  } 
   out <- NULL
   if (!is.null(set)){
     if (set == "prefab"){
       out <- c("AutoArima", "ESSS", "nbGARCH", "pevGARCH")
+    } else{
+      stop("`models` not defined for that `set`")
     }
   }
   if (!is.null(add)){
+    if (!is.character(add)){
+      stop("`add` is not a character")
+    }
     out <- c(out, add)
   }
   classy(out, c("character", "models"))
@@ -27,13 +40,19 @@ models <- function(set = "prefab", add = NULL){
 
 #' @title Write the template for a model into model subdirectory
 #'
-#' @description Create template script for a given model
+#' @description \code{write_model}: Create template script (as written by 
+#'   \code{model_template}) for a given model
 #'
-#' @param options_model model option list
+#' @param options_model A class-\code{model_options} \code{list} of
+#'   options used to set up a general model script. See 
+#'   \code{\link{options_model}}.
 #'
 #' @export
 #'
 write_model <- function(options_model = model_options()){
+  if (!("model_options" %in% class(options_model))){
+    stop("`options_model` is not a model_options list")
+  }
   model <- options_model$name
   if (is.null(model)){
     return()
@@ -46,18 +65,21 @@ write_model <- function(options_model = model_options()){
   write(mod_template, mod_path)
 }
 
-#' @title Create the text to be written out to a script for a model
+#' @rdname write_model
 #'
-#' @description Create the text for a model script to be housed in the model 
-#'   directory
+#' @description \code{model_template}: create the \code{character}-valued
+#'   text for a model script to be housed in the model directory, as written
+#'   out by \code{write_model}.
 #'
-#' @param options_model list of options for the specific model
-#'
-#' @return Model script text
+#' @return \code{model_template}: model script text as a single 
+#'   \code{character} value.
 #'
 #' @export
 #'
 model_template <- function(options_model = model_options()){
+  if (!("model_options" %in% class(options_model))){
+    stop("`options_model` is not a model_options list")
+  }
   tree <- options_model$tree
   if (any(!(tree$subs %in% subdirs()))){
     addl <- which(!(tree$treesubs %in% subdirs()))
@@ -99,5 +121,3 @@ options_model$name, '", metadata, sub_path(tree, "tmp"))'
 )
 
 }
-
-
--- tests/testthat/test-14-prepare_models.R
+++ tests/testthat/test-14-prepare_models.R
@@ -1,1 +1,18 @@
 context("Test prepare_models functions")
+
+test_that("models", {
+  expect_is(models(), "character")
+  expect_is(models(), "models")
+  expect_error(models(1), "`set` is not")
+  expect_error(models(add = 1), "`add` is not")
+  expect_error(models(set = c("prefab", "prefab")), "`set` can only be")
+  expect_error(models(set = "ok"), "not defined for that `set`")
+})
+
+test_that("write_model", {
+  expect_error(write_model(1), "`options_model` is not")
+})
+
+test_that("model_template", {
+  expect_error(write_model(1), "`options_model` is not")
+})
