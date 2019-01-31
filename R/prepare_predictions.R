15e23d757f6e31f8e336907a00b7fb436b67aed5
DAPPERstats <dapperstats@gmail.com>
2019-01-30 20:09:00 -0800

    Update prepare_predictions.R

--- R/prepare_predictions.R
+++ R/prepare_predictions.R
@@ -25,16 +25,16 @@ download_predictions <- function(tree = dirtree(), download = TRUE){
   from1 <- "https://api.github.com/repos/weecology/portalPredictions/"
   from2 <- "contents/predictions"
   from_path <- paste0(from1, from2)
+  from3 <- "https://raw.github.com/weecology/portalPredictions/master/"
   to_path_choices <- sub_paths(tree)
   to_path <- to_path_choices[grepl("predictions", to_path_choices)]
-  to_fpaths <- file_path(tree, filelist)
 
   if (download){
     req <- GET(from_path)
     stop_for_status(req)
     filelist <- unlist(lapply(content(req), "[", "path"), use.names = FALSE)
-    from1 <- "https://raw.github.com/weecology/portalPredictions/master/"
-    from_fpaths <- paste0(from1, filelist)
+    to_fpaths <- file_path(tree, filelist)
+    from_fpaths <- paste0(from3, filelist)
     x <- mapply(download.file, url = from_fpaths, destfile = to_fpaths, 
                 quiet = TRUE)
   }
