15ced47a00cf83cae8ff7b4ebcc77ba81350166d
Juniper Simonis <dapperstats@gmail.com>
2019-08-09 23:21:25 -0700

    Update test-65-prepare_moons.R

--- tests/testthat/test-65-prepare_moons.R
+++ tests/testthat/test-65-prepare_moons.R
@@ -2,6 +2,7 @@ context("Test prepare_moons functions")
 
 unlink(main_path(main = "./testing"), recursive = TRUE, force = TRUE)
 create_dir(main = "./testing")
+  fill_raw(main = "./testing")
   moons <- prep_moons(main = "./testing")
 
 test_that("prep_moons", {
