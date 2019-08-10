ed1cd7d64081a26018a39f07946ef6e999d465cd
Juniper Simonis <dapperstats@gmail.com>
2019-08-09 23:01:04 -0700

    Update test-20-download.R

--- tests/testthat/test-20-download.R
+++ tests/testthat/test-20-download.R
@@ -34,7 +34,7 @@ test_that("zenodo_downloads", {
   expect_is(zenodo_downloads(c("1215988", "833438")), "list")
   expect_is(zenodo_downloads(rec_id = "12345"), "list")
 })
-
+print(verify_raw_data(main = "./testing"))
 test_that("verify_raw_data", {
   expect_equal(verify_raw_data(main = "./testing"), TRUE)
 })
