62a784cc45b3b5ea1f6cf2077afc1bdcb83c74f3
Juniper Simonis <dapperstats@gmail.com>
2019-08-14 12:51:36 -0700

    Update test-40-cast_covariates.R

--- tests/testthat/test-40-cast_covariates.R
+++ tests/testthat/test-40-cast_covariates.R
@@ -1,14 +1,14 @@
 
-context("Test prepare_covariates functions")
+context("Test cast_covariates functions")
 
 test_that("download_climate_forecasts", {
   skip_on_cran() # downloads take too long for cran checks
-  expect_is(download_climate_casts(main=main), "character")
+  expect_is(download_climate_casts(main = "./testing"), "character")
 })
 
 
 test_that("read_climate_forecasts", {
   skip_on_cran() # downloads take too long for cran checks
-  expect_is(read_climate_casts(main = main), "data.frame")
+  expect_is(read_climate_casts(main = "./testing"), "data.frame")
 })
 
