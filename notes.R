afe79a43f654d03285519de2a6d7509f53ac694a
DAPPERstats <dapperstats@gmail.com>
2019-08-22 18:40:48 -0700

    adding the old vignette files in
    edits to come

--- notes.R
+++ notes.R
@@ -13,6 +13,9 @@ devtools::test(filter = "19")
 
 
 # to do
+update ifnull like ifna
+add timestamp to forecast filenames
+soften model running errors 
 # update vignettes
 # add morgans model
 # update pkgdown site
@@ -23,39 +26,7 @@ devtools::test(filter = "19")
  # I WILL NEED TO MANAGE SOMETHING WITH THE ARCHIVED FILE NAME CHANGE
 # the col name is also updated to cast_newmoon
 
-# important notes for news
-#  directory tree structure simplified
-#   dirtree is no more, even tho the structure is still there
-#   base is no more, if you want to make that structure use main = "./name"
-#  download capacity generalized
-#   any zenodo record or concept; any ready-to-go url; (will add any github)
-#  PortalData has been removed as a sub and replaced with "raw", which 
-#   includes all unzipped raw downloads (portal data and portal predictions)
-#  users adding models should permanently add code to model_script_controls()
-#   rather than write their own control functions
-# all the NMME options! :D
-# covariate forecasts are now saved in the raw folder for reference 
-#   rather than only ever loaded within R
-# (mostly) internal generalization to cast 
-#    (there were still lots of "fcast" names
-# adding a new setup of the rodents data is possible now through direct
-#   arguments basically to the summarize rodents function
-#   users can set their own controls as they need
-#   but permanent additions can also be made by adding a tmnt_type to 
-#   rodents_control(s)
-# cast_type has been removed as an input, it's auto determined now based on 
-#  end_moon and the last moon available (if they're equal it's a forecast, 
-#  if not, it's a hindcast)
-#
-# hindcasts are now not skipping from incomplete/missed samples 
-work to get the arg_checks arg passed through the code cleanly!
 
-some basic utilities and
-the prefab model functions dont have arg_checks capability, intentionally
-
-start_newmoon is now start_moon
-
-update ifnull like ifna
 
 
 
