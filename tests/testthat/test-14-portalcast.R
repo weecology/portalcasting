486dbcb5d1326e4168b8e2fd2b5fe014c44e6bbf
DAPPERstats <dapperstats@gmail.com>
2019-08-28 13:02:40 -0700

    Update test-14-portalcast.R

--- tests/testthat/test-14-portalcast.R
+++ tests/testthat/test-14-portalcast.R
@@ -2,15 +2,9 @@ context("Test portalcast functions")
 
 test_that("portalcast", {
   skip_on_cran() # downloads and casting take too long to run on cran
-  expect_message(portalcast(main = "./testing", models = "AutoArima",
-                            cleanup = FALSE))
+  expect_message(portalcast(main = "./testing", models = "AutoArima"))
 })
 
-test_that("prep_data", {
-  skip_on_cran() # downloads and casting take too long to run on cran
-  expect_message(prep_data(main = "./testing", end_moon = 515,
-                            cleanup = FALSE))
-})
 
 
 
