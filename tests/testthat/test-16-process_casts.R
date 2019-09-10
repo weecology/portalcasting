673ba4cbdffedb7668ce5338b6a1a1d731fbc6d9
Juniper Simonis <dapperstats@gmail.com>
2019-09-10 13:06:48 -0700

    Update test-16-process_casts.R

--- tests/testthat/test-16-process_casts.R
+++ tests/testthat/test-16-process_casts.R
@@ -4,11 +4,17 @@ main <- "./testing"
 
 test_that("read_cast_tab", {
   skip_on_cran() # downloads take too long for cran checks
+  expect_is(read_cast_tab(main = main, cast_id = NULL), "data.frame")
   expect_error(read_cast_tab(main = main, cast_id = 1e10))
 })
 
+test_that("read_cast_metadata", {
+  skip_on_cran() # downloads take too long for cran checks
+  expect_is(read_cast_metadata(main = main, cast_id = NULL), "list")
+  expect_error(read_cast_metadata(main = main, cast_id = 1e10))
+})
 
-test_that("read_cast_tab", {
+test_that("add_to_cast_tab", {
   skip_on_cran() # downloads take too long for cran checks
    cast_tab <- read_cast_tab(main = main, cast_id = 1)
    expect_is(add_lead_to_cast_tab(main = main, cast_tab = cast_tab),
