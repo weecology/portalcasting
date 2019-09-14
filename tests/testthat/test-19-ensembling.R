fb24fd5be65ad9713239ca0bdfa9076878fcdd84
Juniper Simonis <dapperstats@gmail.com>
2019-09-14 16:51:29 -0700

    Update test-19-ensembling.R

--- tests/testthat/test-19-ensembling.R
+++ tests/testthat/test-19-ensembling.R
@@ -4,6 +4,7 @@ main <- "./testing"
 
 test_that("ensemble_casts", {
   skip_on_cran() # downloads take too long for cran checks
+  portalcast(main = main, models = c("AutoArima", "ESSS"), end_moons = 515)
   expect_is(ensemble_casts(main = main), "data.frame")
 
   expect_error(ensemble_casts(main = main, cast_id = 1e10))
