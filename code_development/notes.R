2a110f4b1be283dc1b52a7bcd3a5ed7dd71c6f26
juniperlsimonis <dapperstats@gmail.com>
2022-04-01 14:51:00 -0700

    Update notes.R

--- code_development/notes.R
+++ code_development/notes.R
@@ -14,27 +14,7 @@ portalcast(main)
 
 # all functions in production work as used!
 
-####    updating tests
-
-
-
-
-main <- "./testing"
-setup_dir(main)
-portalcast(main, models = "ESSS")
-
-plot_cast_ts(main = main, data_set = "controls")
-plot_cast_point(main = main, data_set = "controls")
-most_ab <- most_abundant_species(main = main, data_set = "controls")
-for(i in 1:3){
-  plot_cast_ts(main = main, data_set = "controls", species = most_ab[i])
-}
-plot_casts_err_lead(main = main)
-plot_casts_cov_RMSE(main = main)
-
-
-devtools::build()
-devtools::test()
+####    updating tests ... should be good for a full run
 
 
 
