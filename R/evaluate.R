acbb13b7f69832c18be6acbf10e87cccfff4159d
juniperlsimonis <dapperstats@gmail.com>
2022-04-07 14:48:08 -0700

    Update evaluate.R

--- R/evaluate.R
+++ R/evaluate.R
@@ -8,9 +8,6 @@
 #'
 #' @param settings \code{list} of controls for the directory, with defaults set in \code{\link{directory_settings}} that should generally not need to be altered.
 #'
-#' @param cast_group,cast_groups \code{integer} (or integer \code{numeric}) value(s) of the cast group(s) to evaluate, as indexed within the directory in the \code{casts} sub folder. See the casts metadata file (\code{casts_metadata.csv}) for summary information. \cr
-#'  \code{cast_group} can only be a single value, whereas \code{cast_groups} can be multiple.
-#'
 #' @param cast_id,cast_ids \code{integer} (or integer \code{numeric}) value(s) representing the casts of interest for evaluating, as indexed within the directory in the \code{casts} sub folder. See the casts metadata file (\code{casts_metadata.csv}) for summary information. \cr
 #'  \code{cast_id} can only be a single value, whereas \code{cast_ids} can be multiple.
 #'
@@ -26,12 +23,35 @@
 #'
 evaluate_casts <- function (main        = ".", 
                             settings    = directory_settings(), 
-                            cast_groups = NULL, 
                             cast_ids    = NULL,
                             quiet       = FALSE, 
                             verbose     = FALSE) {
 
 
+  casts_to_evaluate <- select_casts(main           = main, 
+                                    settings       = settings,
+                                    cast_ids       = cast_ids)
+
+  if (NROW(casts_to_evaluate) == 0) {
+
+    stop("no casts available for request")
+
+  } else {
+
+    cast_ids   <- casts_to_evaluate$cast_id
+    ncasts_ids <- length(cast_ids)
+
+  }
+
+  for (i in 1:ncast_ids) {
+
+    evaluate_cast(main     = main,
+                  settings = settings,
+                  cast_id  = cast_ids[i],
+                  quiet    = quiet,
+                  verbose  = verbose)
+
+  }
 
   invisible()
 
@@ -44,7 +64,6 @@ evaluate_casts <- function (main        = ".",
 #'
 evaluate_cast <- function (main       = ".", 
                            settings   = directory_settings(), 
-                           cast_group = NULL, 
                            cast_id    = NULL,
                            quiet      = FALSE, 
                            verbose    = FALSE) {
