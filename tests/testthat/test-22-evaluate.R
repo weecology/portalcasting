2c3a5b18e28cf449c319503b4a6cd0dfd01c2b04
juniperlsimonis <dapperstats@gmail.com>
2022-04-07 12:57:23 -0700

    Create test-22-evaluate.R
    placeholder tests for placeholdered functions

---
+++ tests/testthat/test-22-evaluate.R
@@ -0,0 +1,15 @@
+context(desc = "evaluate functions")
+
+test_that(desc = "evaluate_casts evaluates casts", {
+
+  expect_null(evaluate_casts())
+
+})
+
+
+test_that(desc = "evaluate_cast evaluates cast", {
+
+  expect_null(evaluate_cast())
+
+})
+
