ce1a31e3c679a290af48ba782b7fad32de7e0040
juniperlsimonis <dapperstats@gmail.com>
2022-04-07 12:50:11 -0700

    updating docs adding evaluate_cast

--- R/evaluate.R
+++ R/evaluate.R
@@ -2,15 +2,17 @@
 
 #' @title Evaluate Forecasts
 #'
-#' @description Evaluate forecasts in the directory, based on id or group, or (if \code{cast_ids = NULL} and \code{cast_groups = NULL} , the default) evaluate all.
+#' @description Evaluate forecasts in the directory, based on id or group, or (if \code{cast_ids = NULL} and \code{cast_groups = NULL}, the default) \code{evaluate_casts} will evaluate all and (if \code{cast_id = NULL} and \code{cast_group = NULL}, the default), \code{evaluate_cast} will evaluate the most recent cast.
 #'
 #' @param main \code{character} value of the name of the main component of the directory tree.
 #'
 #' @param settings \code{list} of controls for the directory, with defaults set in \code{\link{directory_settings}} that should generally not need to be altered.
 #'
-#' @param cast_groups \code{integer} (or integer \code{numeric}) value of the cast group to combine with an ensemble. If \code{NULL} (default), the most recent cast group is ensembled. 
+#' @param cast_group,cast_groups \code{integer} (or integer \code{numeric}) value(s) of the cast group(s) to evaluate, as indexed within the directory in the \code{casts} sub folder. See the casts metadata file (\code{casts_metadata.csv}) for summary information. \cr
+#'  \code{cast_group} can only be a single value, whereas \code{cast_groups} can be multiple.
 #'
-#' @param cast_ids \code{integer} (or integer \code{numeric}) values representing the casts of interest for restricting ensembling, as indexed within the directory in the \code{casts} sub folder. See the casts metadata file (\code{casts_metadata.csv}) for summary information.
+#' @param cast_id,cast_ids \code{integer} (or integer \code{numeric}) value(s) representing the casts of interest for evaluating, as indexed within the directory in the \code{casts} sub folder. See the casts metadata file (\code{casts_metadata.csv}) for summary information. \cr
+#'  \code{cast_id} can only be a single value, whereas \code{cast_ids} can be multiple.
 #'
 #' @param quiet \code{logical} indicator if progress messages should be quieted.
 #'
@@ -18,19 +20,40 @@
 #'
 #' @return \code{NULL}, \code{\link[base]{invisible}}-ly.
 #'
+#' @name evaluate forecasts
+#'
 #' @export
 #'
-evaluate_casts <- function (main           = ".", 
-                            settings       = directory_settings(), 
-                            cast_groups    = NULL, 
-                            cast_ids       = NULL,
-                            quiet          = FALSE, 
-                            verbose        = FALSE) {
+evaluate_casts <- function (main        = ".", 
+                            settings    = directory_settings(), 
+                            cast_groups = NULL, 
+                            cast_ids    = NULL,
+                            quiet       = FALSE, 
+                            verbose     = FALSE) {
 
 
 
   invisible()
 
 }
+
+#'
+#' @rdname evaluate-forecasts
+#'
+#' @export
+#'
+evaluate_cast <- function (main       = ".", 
+                           settings   = directory_settings(), 
+                           cast_group = NULL, 
+                           cast_id    = NULL,
+                           quiet      = FALSE, 
+                           verbose    = FALSE) {
+
+
+
+  invisible()
+
+}
+
 
 
