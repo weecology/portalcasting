d3c37e404fc4b6d5c30d1d6df93411d38a744b31
juniperlsimonis <dapperstats@gmail.com>
2022-04-07 17:38:56 -0700

    Update test-22-evaluate.R

--- tests/testthat/test-22-evaluate.R
+++ tests/testthat/test-22-evaluate.R
@@ -1,15 +1,17 @@
 context(desc = "evaluate functions")
 
+main <- "./testing"
+
 test_that(desc = "evaluate_casts evaluates casts", {
 
-  expect_null(evaluate_casts())
+  expect_null(evaluate_casts(main = main))
 
 })
 
 
 test_that(desc = "evaluate_cast evaluates cast", {
 
-  expect_null(evaluate_cast())
+  expect_null(evaluate_cast(main = main))
 
 })
 
