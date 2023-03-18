05d58518ea24c8427e3175baf542f843076edf64
juniperlsimonis <dapperstats@gmail.com>
2023-03-18 13:29:56 -0700

    Update test-03-paths.R

--- tests/testthat/test-03-paths.R
+++ tests/testthat/test-03-paths.R
@@ -9,11 +9,3 @@ test_that(desc = "file_ext extracts the file extension",
   expect_equal(file_ext(NMME_urls()[[1]], "="), "csv")
 
 })
-
-test_that(desc = "path_no_ext extracts the path without the file extension",
-          code = {
-
-  expect_equal(path_no_ext("home/folders.with.dots/stuff/ok.csv"),
-                           "home/folders.with.dots/stuff/ok")
-
-})
