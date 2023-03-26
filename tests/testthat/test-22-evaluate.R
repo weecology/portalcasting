783b5d1f3ed1649cb60d0ff09f62a5d65d1ad467
juniperlsimonis <dapperstats@gmail.com>
2023-03-26 14:20:16 -0700

    Update test-22-evaluate.R

--- tests/testthat/test-22-evaluate.R
+++ tests/testthat/test-22-evaluate.R
@@ -4,16 +4,16 @@ main <- "./testing"
 
 test_that(desc = "evaluate_casts evaluates casts", {
 
-  #expect_is(evaluate_casts(main = main, cast_ids = c("1-1", "1-2")), "list")
-  #expect_error(evaluate_casts(main = main, cast_ids = c(1e100, "1-2")))
-expect_equal(1, 1)
+  expect_is(evaluate_casts(main = main, cast_ids = c("1-1", "1-2")), "list")
+  expect_error(evaluate_casts(main = main, cast_ids = c(1e100, "1-2")))
+
 })
 
 
 test_that(desc = "evaluate_cast evaluates cast", {
 
-  #expect_is(evaluate_cast(main = main, cast_id = "1-1"), "data.frame")
-  #expect_error(evaluate_cast(main = main, cast_id = 1e100))
-expect_equal(1, 1)
+  expect_is(evaluate_cast(main = main, cast_id = "1-1"), "data.frame")
+  expect_error(evaluate_cast(main = main, cast_id = 1e100))
+
 })
 
