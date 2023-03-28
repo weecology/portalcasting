fe00a2c9684b54a3ccf5bb253d3ab0f5f0f8591b
juniperlsimonis <dapperstats@gmail.com>
2023-03-27 18:49:46 -0700

    Update test-22-evaluate.R

--- tests/testthat/test-22-evaluate.R
+++ tests/testthat/test-22-evaluate.R
@@ -5,6 +5,9 @@ main <- "./testing"
 test_that(desc = "evaluate_casts evaluates casts", {
 
   cast_ids <- select_casts(main)$cast_id
+  nids <- length(cast_ids)
+  nsample_ids <- 100
+  cast_ids <- cast_ids[round(seq(1, nids, length.out = nsample_ids))]
   expect_is(evaluate_casts(main = main, cast_ids = cast_ids), "data.frame")
   expect_error(evaluate_casts(main = main, cast_ids = c(1e100, 2e100)))
 
