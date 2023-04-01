19c7d83211b09106c3e116c6663720dc928ea423
Juniper Simonis <dapperstats@gmail.com>
2023-03-31 23:07:30 -0700

    o

--- code_development/todo.R
+++ code_development/todo.R
@@ -1,3 +1,9 @@
+build test checks ugh
+
+main <- "~/pc"
+devtools::document()
+setup_sandbox(main)
+
 
 once portalr update is integrated
 remotes::install_github("weecology/portalr#288")
--- tests/testthat/test-11-prepare_metadata.R
+++ tests/testthat/test-11-prepare_metadata.R
@@ -8,6 +8,7 @@ test_that(desc = "prepare_metadata", {
 
     skip_on_cran() 
 
+  prepare_rodents(main = main)
 
   md <- prepare_metadata(main = main)
   expect_is(md, "list")
