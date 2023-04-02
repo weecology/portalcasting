4128050f44fb4747aa6b42fdceee909ea71fd093
juniperlsimonis <dapperstats@gmail.com>
2023-04-02 14:55:44 -0700

    g

--- code_development/tests.R
+++ code_development/tests.R
@@ -8,8 +8,13 @@ devtools::test(filter = "20")
 devtools::test(filter = "21")
 devtools::test(filter = "22")
 devtools::test(filter = "23")
+devtools::test(filter = "24")
+devtools::test(filter = "25")
+devtools::test(filter = "26")
+devtools::test(filter = "27")
 
 devtools::test(filter = "30")
+devtools::test(filter = "31")
 
 devtools::test(filter = "40")
 
---
+++ tests/testthat/test-26-prepare_metdata.R
@@ -0,0 +1,18 @@
+context(desc = "metadata prepping functions")
+
+main1 <- file.path(tempdir(), "testing1")
+main2 <- file.path(tempdir(), "testing2")
+main3 <- file.path(tempdir(), "testing3")
+
+test_that(desc = "prepare_metadata", {
+
+  # downloads take too long for cran checks
+
+  skip_on_cran() 
+
+  prepare_rodents(main = main2)
+
+  md <- prepare_metadata(main = main2)
+  expect_is(md, "list")
+
+})
