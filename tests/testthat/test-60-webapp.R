9822a0791fc0b33bd93ef6c37fc8bf3015ed9494
juniperlsimonis <dapperstats@gmail.com>
2023-04-11 13:12:54 -0700

    Update test-60-webapp.R

--- tests/testthat/test-60-webapp.R
+++ tests/testthat/test-60-webapp.R
@@ -23,13 +23,22 @@ test_that(desc = "global_list makes a list of globals and component functions op
 
   expect_is(available_models(global = gl, event = "initial_forecast_tab"), "character")
   expect_is(available_models(global = gl, event = "initial_evaluation_tab"), "character")
+  expect_is(available_models(global = gl, event = "null_test"), "NULL")
+
   expect_is(available_datasets(global = gl, event = "initial_forecast_tab"), "character")
   expect_is(available_datasets(global = gl, event = "initial_evaluation_tab"), "character")
+  expect_is(available_datasets(global = gl, event = "null_test"), "NULL")
+
   expect_is(available_species(global = gl, event = "initial_forecast_tab"), "character")
   expect_is(available_species(global = gl, event = "initial_evaluation_tab"), "character")
+  expect_is(available_species(global = gl, event = "null_test"), "NULL")
+
   expect_is(available_historic_end_newmoonnumbers(global = gl, event = "initial_forecast_tab"), "integer")
   expect_is(available_historic_end_newmoonnumbers(global = gl, event = "initial_evaluation_tab"), "integer")
+  expect_is(available_historic_end_newmoonnumbers(global = gl, event = "null_test"), "NULL")
+
   expect_is(available_newmoonnumbers(global = gl, event = "initial_evaluation_tab"), "integer")
+  expect_is(available_newmoonnumbers(global = gl, event = "null_test"), "NULL")
 
   expect_is(available_models(global = gl, event = "forecast_tab_model", rv = rv), "character")
   expect_is(available_models(global = gl, event = "evaluation_tab_model", rv = rv), "character")
@@ -50,24 +59,29 @@ test_that(desc = "global_list makes a list of globals and component functions op
   expect_is(selected_model(global = gl, event = "forecast_tab_model", rv = rv1), "character")
   rv1 <- update_list(rv, evaluation_tab_model = "xx")
   expect_is(selected_model(global = gl, event = "evaluation_tab_model", rv = rv1), "character")
+  expect_is(selected_model(global = gl, event = "null_test", rv = rv1), "NULL")
 
   rv1 <- update_list(rv, forecast_tab_dataset = "xx")
   expect_is(selected_dataset(global = gl, event = "forecast_tab_dataset", rv = rv1), "character")
   rv1 <- update_list(rv, evaluation_tab_dataset = "xx")
   expect_is(selected_dataset(global = gl, event = "evaluation_tab_dataset", rv = rv1), "character")
+  expect_is(selected_dataset(global = gl, event = "null_test", rv = rv1), "NULL")
 
   rv1 <- update_list(rv, forecast_tab_species = "xx")
   expect_is(selected_species(global = gl, event = "forecast_tab_species", rv = rv1), "character")
   rv1 <- update_list(rv, evaluation_tab_species = "xx")
   expect_is(selected_species(global = gl, event = "evaluation_tab_species", rv = rv1), "character")
+  expect_is(selected_species(global = gl, event = "null_test", rv = rv1), "NULL")
 
   rv1 <- update_list(rv, forecast_tab_historic_end_newmoonnumber = 1e10)
   expect_is(selected_historic_end_newmoonnumber(global = gl, event = "forecast_tab_historic_end_newmoonnumber", rv = rv1), "integer")
   rv1 <- update_list(rv, evaluation_tab_historic_end_newmoonnumber = 1e10)
-  expect_is(selected_historic_end_newmoonnumber(global = gl, event = "evaluation_tab_historic_end_newmoonnumber", rv = rv1), "integer")
+  expect_is(selected_species(global = gl, event = "evaluation_tab_species", rv = rv1), "character")
+  expect_is(selected_species(global = gl, event = "null_test", rv = rv1), "NULL")
 
   rv1 <- update_list(rv, evaluation_tab_newmoonnumber = 1e10)
   expect_is(selected_newmoonnumber(global = gl, event = "evaluation_tab_newmoonnumber", rv = rv1), "integer")
+  expect_is(selected_newmoonnumber(global = gl, event = "null_test", rv = rv1), "NULL")
 
 
 
