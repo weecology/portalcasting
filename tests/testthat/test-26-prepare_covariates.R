048f3950bc6dcf93b24f0778dc9e2e3d22564f99
juniperlsimonis <dapperstats@gmail.com>
2023-04-16 22:44:15 -0700

    Update test-26-prepare_covariates.R

--- tests/testthat/test-26-prepare_covariates.R
+++ tests/testthat/test-26-prepare_covariates.R
@@ -9,7 +9,7 @@ test_that(desc = "prepare_covariates", {
 
   skip_on_cran() 
 
-  covs <- prepare_covariates(main = main2)
+  covs <- prepare_covariates(main = main3)
   expect_is(covs, "data.frame")
 
 })
