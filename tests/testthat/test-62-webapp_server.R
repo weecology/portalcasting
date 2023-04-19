403ad39904680a3a563ea02ee3091319cbe02a82
juniperlsimonis <dapperstats@gmail.com>
2023-04-18 18:21:24 -0700

    Update test-62-webapp_server.R

--- tests/testthat/test-62-webapp_server.R
+++ tests/testthat/test-62-webapp_server.R
@@ -39,5 +39,110 @@ test_that(desc = "server functions work off of global list", {
   expect_silent(uo2 <- update_output(main = main3, event = "evaluation_tab", rv = rv, input = rv, output = io))
   expect_is(uo2, "list")
 
+
+  testServer(app_server, {
+
+    session$setInputs(forecast_tab_species                       = gl$initial_forecast_tab_selected_species,
+                      forecast_tab_dataset                       = gl$initial_forecast_tab_selected_dataset,
+                      forecast_tab_model                         = gl$initial_forecast_tab_selected_model,
+                      forecast_tab_historic_end_newmoonnumber    = gl$initial_forecast_tab_selected_historic_end_newmoonnumber,
+                      evaluation_tab_species                     = gl$initial_evaluation_tab_selected_species,
+                      evaluation_tab_dataset                     = gl$initial_evaluation_tab_selected_dataset,
+                      evaluation_tab_model                       = gl$initial_evaluation_tab_selected_model,
+                      evaluation_tab_historic_end_newmoonnumber  = gl$initial_evaluation_tab_selected_historic_end_newmoonnumber,
+                      evaluation_tab_newmoonnumber               = gl$initial_evaluation_tab_selected_newmoonnumber)
+
+    expect_equal(output$forecast_tab_species, as.character(gl$initial_forecast_tab_selected_species))
+    expect_equal(output$forecast_tab_dataset, gl$initial_forecast_tab_selected_dataset)
+    expect_equal(output$forecast_tab_model, as.character(gl$initial_forecast_tab_selected_model))
+    expect_equal(output$forecast_tab_historic_end_newmoonnumber, as.character(gl$initial_forecast_tab_selected_historic_end_newmoonnumber))
+    expect_equal(output$evaluation_tab_species, as.character(gl$initial_evaluation_tab_selected_species))
+    expect_equal(output$evaluation_tab_dataset, gl$initial_evaluation_tab_selected_dataset)
+    expect_equal(output$evaluation_tab_model, as.character(gl$initial_evaluation_tab_selected_model))
+    expect_equal(output$evaluation_tab_historic_end_newmoonnumber, as.character(gl$initial_evaluation_tab_selected_historic_end_newmoonnumber))
+    expect_equal(output$evaluation_tab_newmoonnumber, as.character(gl$initial_evaluation_tab_selected_newmoonnumber))
+
+    output <- initial_output(main = main3, rv = rv, output = output)
+
+    expect_equal(names(output$forecast_tab_ts_plot), c("src", "width", "height", "alt", "coordmap"))
+    expect_equal(names(output$forecast_tab_ss_plot), c("src", "width", "height", "alt", "coordmap"))
+    expect_equal(names(output$evaluation_tab_sp_plot), c("src", "width", "height", "alt", "coordmap"))
+    expect_equal(names(output$evaluation_tab_RMSE_plot), c("src", "width", "height", "alt", "coordmap"))
+    expect_equal(names(output$covariates_tab_ndvi_plot), c("src", "width", "height", "alt", "coordmap"))
+    expect_equal(names(output$covariates_tab_precip_plot), c("src", "width", "height", "alt", "coordmap"))
+    expect_equal(names(output$covariates_tab_temp_plot), c("src", "width", "height", "alt", "coordmap"))
+
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "forecast_tab_species", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "forecast_tab_dataset", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "forecast_tab_model", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "forecast_tab_historic_end_newmoonnumber", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "evaluation_tab_species", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "evaluation_tab_dataset", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "evaluation_tab_model", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "evaluation_tab_historic_end_newmoonnumber", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+    expect_equal(event_reaction(main = main3,
+                                global = gl,
+                                event = "evaluation_tab_newmoonnumber", 
+                                rv = rv, 
+                                input = input, 
+                                output = output, 
+                                session = session), NULL)
+
+  })
+
+
 })
+
+
+
+
 
