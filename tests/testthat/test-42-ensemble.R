5545a402534bcef582a3582b8871a61653c5cd10
Juniper Simonis <dapperstats@gmail.com>
2023-04-20 18:42:16 -0700

    Update test-42-ensemble.R

--- tests/testthat/test-42-ensemble.R
+++ tests/testthat/test-42-ensemble.R
@@ -12,8 +12,8 @@ test_that(desc = "ensemble_forecasts ensembles accordingly", {
   forecast_id <- forecast_ids[length(forecast_ids)]
 
   expect_is(ensemble_forecasts(main     = main3, 
-                           forecast_ids = forecast_id), "data.frame")
-  expect_error(ensemble_forecasts(main = main3, forecast_id = 1e10))
+                               forecasts_ids = forecast_id), "data.frame")
+  expect_error(ensemble_forecasts(main = main3, forecasts_ids = 1e10))
 
   expect_error(ensemble_forecasts(main = main3, historic_end_newmoonnumber = 1e10))
 
