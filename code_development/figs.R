d738776c2d730f228e9a8ca3dfa6d4cca91be633
Juniper Simonis <dapperstats@gmail.com>
2023-04-24 19:27:44 -0700

    s

--- code_development/figs.R
+++
@@ -1,7 +0,0 @@
-main <- "~/pct"
-plot_cast_ts(main)
-
-
-plot_casts_err_lead(main)
-
-plot_casts_cov_RMSE(main)
--- code_development/todo.R
+++
@@ -1,8 +0,0 @@
-
-once portalr update is integrated
-remotes::install_github("weecology/portalr#288")
-can get removed
-
-anything to add to docker image for arrow?
-
-
