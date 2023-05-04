2418f64589e497704073258144b108d5156ab9fb
juniperlsimonis <dapperstats@gmail.com>
2023-05-04 01:02:44 -0700

    Update app.R

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,3 +1,14 @@
+
+main_exists   <- any(ls() == "main")
+
+onStop(function() {
+  if (!main_exists) {
+    rm(main, inherits = TRUE)
+  }
+  rm(global, inherits = TRUE)
+
+})
+
 main   <<- dynGet("main")
 global <<- global_list(main = main)
 
