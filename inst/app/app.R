8d9f293978110dbc71f66ae434cee2d7664da4bc
henrykironde <henrykironde@gmail.com>
2023-05-05 00:41:27 -0400

    Add load library
    check if object exists

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,15 +1,22 @@
+library(portalcasting)
 
 main_exists   <- any(ls() == "main")
 
 onStop(function() {
-  if (!main_exists) {
+  if (exists('main_exists')) {
     rm(main, inherits = TRUE)
   }
-  rm(global, inherits = TRUE)
-
+  if (exists('global')) {
+    rm(global, inherits = TRUE)
+  }
 })
 
-main   <<- dynGet("main")
+if (!main_exists) {
+  main   <<- "."
+} else {
+  main   <<- dynGet("main")
+}
+
 global <<- global_list(main = main)
 
 shinyApp(ui     = app_ui(global = global),
